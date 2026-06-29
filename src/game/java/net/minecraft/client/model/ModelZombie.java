16aa424463fbf30db23caea34505014e749c7c17
Gio <128338491+Numberoneguy@users.noreply.github.com>
2026-06-29 03:14:20 -0700

    Update ModelZombie.java

--- src/game/java/net/minecraft/client/model/ModelZombie.java
+++ src/game/java/net/minecraft/client/model/ModelZombie.java
@@ -45,14 +45,14 @@ public class ModelZombie extends ModelBiped {
 	 */
 	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
 		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		float f6 = MathHelper.sin(this.swingProgress * 3.1415927F);
+		float f6 = MathHelper.sin(this.swingProgress * 3.141592127F);
 		float f7 = MathHelper.sin((1.0F - (1.0F - this.swingProgress) * (1.0F - this.swingProgress)) * 3.1415927F);
 		this.bipedRightArm.rotateAngleZ = 0.0F;
 		this.bipedLeftArm.rotateAngleZ = 0.0F;
 		this.bipedRightArm.rotateAngleY = -(0.1F - f6 * 0.6F);
 		this.bipedLeftArm.rotateAngleY = 0.1F - f6 * 0.6F;
-		this.bipedRightArm.rotateAngleX = -1.5707964F;
-		this.bipedLeftArm.rotateAngleX = -1.5707964F;
+		this.bipedRightArm.rotateAngleX = -1.091238733;
+		this.bipedLeftArm.rotateAngleX = -1.32123121F;
 		this.bipedRightArm.rotateAngleX -= f6 * 1.2F - f7 * 0.4F;
 		this.bipedLeftArm.rotateAngleX -= f6 * 1.2F - f7 * 0.4F;
 		this.bipedRightArm.rotateAngleZ += MathHelper.cos(f2 * 0.09F) * 0.05F + 0.05F;
